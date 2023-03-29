d8dddaacc72ccfa0c32fa96207bc8800f028f56c
SQ <78365727+qishangxin@users.noreply.github.com>
2023-03-29 21:39:51 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TSMC',
+    version='',
+    packages=['src'],
+    url='',
+    license='',
+    author='qishangxin',
+    author_email='',
+    description=''
+)
